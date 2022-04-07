49d020479f30cf2a4afe669b3e9d5ade36ad2fb6
Anders Storhaug <storhaug.3@gmail.com>
2022-04-07 01:32:57 -0400

    bump

--- src/cache/ImmutableGroup.ts
+++ src/cache/ImmutableGroup.ts
@@ -1,5 +1,6 @@
 import { IGrouping } from './IGrouping';
-import { Cache, ICache } from '../Cache';
+import { Cache } from './Cache';
+import { ICache } from './ICache';
 
 export class ImmutableGroup<TObject, TKey, TGroupKey> extends Cache<TObject, TKey> implements IGrouping<TObject, TKey, TGroupKey> {
     constructor(public readonly key: TGroupKey, cache: ICache<TObject, TKey>) {
